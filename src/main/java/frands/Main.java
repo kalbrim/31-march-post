5782b75bec26adb310fa205bf460d86ad61998cb
Brendan Comiskey <comiskeybrendan@gmail.com>
2023-03-31 10:04:37 -0400

    try sven

---
+++ src/main/java/frands/Main.java
@@ -0,0 +1,26 @@
+package frands;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("just a town he lives in");
+    }
+
+//    public ListNode detectCycle(ListNode head) {
+//
+//        ListNode slow = head, fast = head;
+//        while (fast != null && fast.next != null) {
+//            slow = slow.next;
+//            fast = fast.next.next;
+//            if (slow == fast) {
+//                ListNode slow2 = head;
+//                while (slow2 != slow) {
+//                    slow2 = slow2.next;
+//                    slow = slow.next;
+//                }
+//                return slow2;
+//            }
+//        }
+//        return null;
+//
+//    }
+}
